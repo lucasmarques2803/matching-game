2d9f76da8bc558e998c378e07b86dac2aee30c0e
Lucas Marques <lucas-marques2003@hotmail.com>
2023-03-30 11:24:14 -0300

    :sparkles: Finish the BoardGame object

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,7 +1,15 @@
+import "./style.css";
 import CardGame from "../../components/CardGame";
 
 function BoardGame(amountCards) {
-    return CardGame().repeat(amountCards);
+    const $htmlCardGame = CardGame();
+    const $htmlContent = $htmlCardGame.repeat(amountCards);
+
+    return `
+        <section class="board-game">
+            ${$htmlContent}
+        </section>
+    `;
 };
 
 export default BoardGame;
