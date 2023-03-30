22440fa112d578c5463084bab033e47a402195df
Lucas Marques <lucas-marques2003@hotmail.com>
2023-03-30 11:58:54 -0300

    :construction: Create the ScoreBoard object without CSS

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
+import ScoreBoard from "./src/objects/ScoreBoard";
 import BoardGame from "./src/objects/BoardGame";
-import PlayerName from "./src/components/PlayerName";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML(
     "beforeend",
     `
-        ${PlayerName("Player1")}
-        ${PlayerName("Player2")}
+        ${ScoreBoard()}
         ${BoardGame(6)}
     `
 );
