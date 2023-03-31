1bf3b8e56e4922130cc88ea3417bdd1292eb73af
Lucas Marques <lucas-marques2003@hotmail.com>
2023-03-31 17:16:31 -0300

    💄 Add CSS to PlayerScore component

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -1,9 +1,11 @@
+import "./style.css";
+
 function PlayerScore(points = 0) {
     return /*html*/`
-        <ol data-points="${points}">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+        <ol class="player-score" data-points="${points}">
+            <li class="pointer">1</li>
+            <li class="pointer">2</li>
+            <li class="pointer">3</li>
         </ol>
     `;
 };
