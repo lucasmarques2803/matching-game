fbd251815dc7c640e4faf72c6968bd66d1971a27
Lucas Marques <lucas-marques2003@hotmail.com>
2023-03-31 17:57:32 -0300

    ✨ Create different cards

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import CardGame from "../CardGame";
 
-function CardFrontBack() {
+function CardFrontBack(icon, altIcon) {
     window.cardFrontBack = {}
     window.cardFrontBack.handleClick = (event) => {
         const $origin = event.target;
@@ -17,7 +17,7 @@ function CardFrontBack() {
             </div>
 
             <div class="card -back">
-                ${CardGame("javascript", "Logo do JavaScript")}
+                ${CardGame(icon, altIcon)}
             </div>            
         </article>
     `;
---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,26 @@
+const cards = [{
+    icon: "javascript",
+    altIcon: "Logo do JavaScript"
+},
+{
+    icon: "javascript",
+    altIcon: "Logo do JavaScript"
+},
+{
+    icon: "html",
+    altIcon: "Logo do html"
+},
+{
+    icon: "html",
+    altIcon: "Logo do html"
+},
+{
+    icon: "css",
+    altIcon: "Logo do css"
+},
+{
+    icon: "css",
+    altIcon: "Logo do css"
+}];
+
+export default cards
